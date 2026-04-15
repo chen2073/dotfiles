456d17692c5df90d82115962c0cfdc92fdb23343
James Chen <jchen@cpacketnetworks.com>
2026-04-15 16:40:23 -0700

    update comment keymaps

--- nvim/.config/nvim/lua/james/plugins/comment.lua
+++ nvim/.config/nvim/lua/james/plugins/comment.lua
@@ -4,15 +4,15 @@ return {
 		require("Comment").setup({})
 
 		local comment = require("Comment.api")
-		vim.keymap.set("i", "<C-l>", function()
+		vim.keymap.set({ "n", "i" }, "<C-l>", function()
 			comment.toggle.linewise.current()
-		end, { desc = "toggle comment insert mode" })
+		end, { desc = "toggle comment" })
 
-		vim.keymap.set({ "n", "v" }, "<leader>/u", function()
+		vim.keymap.set({ "n", "v" }, "<leader>cu", function()
 			comment.uncomment.linewise()
 		end, { desc = "uncomment current line" })
 
-		vim.keymap.set({ "n", "v" }, "<leader>/c", function()
+		vim.keymap.set({ "n", "v" }, "<leader>cc", function()
 			comment.comment.linewise()
 		end, { desc = "comment current line" })
 	end,
