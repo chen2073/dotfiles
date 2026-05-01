f25788796d01d45535257c0d68489f6d68d3e026
James Chen <jchen@cpacketnetworks.com>
2026-05-01 13:59:01 -0700

    codediff mod

--- nvim/.config/nvim/lua/james/plugins/codediff.lua
+++ nvim/.config/nvim/lua/james/plugins/codediff.lua
@@ -8,19 +8,22 @@ return {
 			view = {
 				next_hunk = "]h", -- Jump to next change
 				prev_hunk = "[h", -- Jump to previous change
-				toggle_stage = "<leader>hS", -- Stage/unstage current file (works in explorer and diff buffers)
-				stage_hunk = "<leader>hs", -- Stage hunk under cursor to git index
-				unstage_hunk = "<leader>hu", -- Unstage hunk under cursor from git index
-				discard_hunk = "<leader>hr", -- Discard hunk under cursor (working tree only)
+				toggle_stage = "<leader>s", -- Stage/unstage current file (works in explorer and diff buffers)
 			},
 			conflict = {
-				accept_incoming = "<leader>ct", -- Accept incoming (theirs/left) change
-				accept_current = "<leader>co", -- Accept current (ours/right) change
-				accept_both = "<leader>cb", -- Accept both changes (incoming first)
-				discard = "<leader>cx", -- Discard both, keep base
 				next_conflict = "]c", -- Jump to next conflict
 				prev_conflict = "[c", -- Jump to previous conflict
 			},
+		},
+	},
+	keys = {
+		{
+			"<leader>v",
+			mode = { "n" },
+			function()
+				vim.cmd("CodeDiff")
+			end,
+			desc = "CodeDiff",
 		},
 	},
 }
